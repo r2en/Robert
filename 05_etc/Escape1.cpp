3a85dc8d4f0b48f6717773126b54dbc3434cb816
xxxHAL <s12161te@gmail.com>
2017-02-05 09:29:28 +0900

    05

---
+++ 05_etc/Escape1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void DumpCode(const char* str){
+	for(int i = 0; str[i] != '\0'; ++i){
+		printf("%02x ",(unsigned char)str[i]);
+	}
+	cout << endl;
+}
+
+int main(){
+	DumpCode("\"\'\?\\");
+	DumpCode("\a\b\t\n\v\f\r");
+	DumpCode("\xAB" "\x0AB" "\xA" "B");
+	DumpCode("c:\new\abc");
+	DumpCode("c:\\new\\abc");
+}
